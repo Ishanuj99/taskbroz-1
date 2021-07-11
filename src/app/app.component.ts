f801ae49d3643a8bff32ccd5af34d94f4b2bb36c
Ishanuj <ishanuj99@gmail.com>
2021-07-11 23:51:51 +0530

    Changes to deploy on heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
-
+import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faRupeeSign } from '@fortawesome/free-solid-svg-icons';
+import { faCommentDots } from '@fortawesome/free-solid-svg-icons';
+import { faFileAlt } from '@fortawesome/free-solid-svg-icons';
+import { faCreditCard } from '@fortawesome/free-solid-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSquare } from '@fortawesome/free-solid-svg-icons';
+import { faIdBadge } from '@fortawesome/free-solid-svg-icons';
+import { faChartLine } from '@fortawesome/free-solid-svg-icons';
+import { faAlignCenter } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +17,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-task1';
+  faCalendarAlt=faCalendarAlt;
+  faHeart=faHeart;
+  faRupeeSign=faRupeeSign;
+  faCommentDots=faCommentDots;
+  faFileAlt=faFileAlt;
+  faCreditCard=faCreditCard;
+  faSearch= faSearch;
+  faSquare=faSquare;
+  faIdBadge=faIdBadge;
+  faChartLine=faChartLine;
+  faAlignCenter=faAlignCenter;
 }
